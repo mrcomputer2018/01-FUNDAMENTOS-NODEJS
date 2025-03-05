92251015fe3d5a47baee9d84364d22db04354608
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-03-05 06:07:04 -0300

    implement async request body parsing and user creation in HTTP server

--- src/server.js
+++ src/server.js
@@ -2,25 +2,46 @@ import http from 'node:http';
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer( async(req, res) => {
     const { url, method } = req;
 
-   
+    const buffer = [];
+
+    //carrega toda a minha stream dentro do array dee buffer
+    for await (const chunk of req) {
+        buffer.push(chunk);
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffer).toString());
+        console.log(body);
+    } catch (error) {
+        req.body = null;
+    }
+
     if (url === '/users' && method === 'GET') {
-      return res
+    return res
         .setHeader('Content-Type', 'application/json')
         .status(200)
         .end(JSON.stringify(users));
     }
 
     if (url === '/users' && method === 'POST') {
-      users.push(
-        { 
-          id: 1, name: 'Lucas doe', email: 'lucas@lucass.com'
-        }
-      );
+        const { name, email } = req.body
 
-      return res.writeHead(201).end();
+        let id = 1;
+
+        if(users.length > 0) {
+            id = users[users.length - 1].id + 1;
+        }
+
+        users.push(
+            { 
+                id, name, email
+            }
+        );
+
+        return res.writeHead(201).end();
     }
 
     res.writeHead(404).end('Not Found');
@@ -28,5 +49,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+    console.log('Server is listening on port 3000');
 });
